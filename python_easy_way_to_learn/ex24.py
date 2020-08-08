707a865f1c87aca786b1366464d654cbe052311c
Arkadiy Voronov <voronov_ao@mail.ru>
2020-08-08 11:37:34 +0500

    add fix

--- python_easy_way_to_learn/ex24.py
+++ python_easy_way_to_learn/ex24.py
@@ -24,8 +24,8 @@ def secret_formula(started):
     jars = jelly_beans / 1000
     crates = jars / 100
     return jelly_beans, jars, crates
-    
-    
+
+
 start_point = 10000
 beans, jars crates = secret_formula(start_point)
 
