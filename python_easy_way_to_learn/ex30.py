764556d51831dddd10c40c9ddec870a94dd1c79e
Arkadiy Voronov <voronov_ao@mail.ru>
2020-08-08 11:44:09 +0500

    add fix

--- python_easy_way_to_learn/ex30.py
+++ python_easy_way_to_learn/ex30.py
@@ -9,14 +9,14 @@ elif cars < people:
     print("Не поедем на машине.")
 else:
     print("Мы не можем выбрать.")
-    
+
 if trucks > cars:
     print("Слишком много автобусов.")
 elif trucks < cars:
     print("Может, поехать на автобусе?")
 else:
     print("Мы до сих пор не можем выбрать.")
-    
+
 if people > trucks:
     print("Хорошо, давайте поедем на автобусе.")
 else:
