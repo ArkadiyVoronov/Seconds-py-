144bf6cb4a5a7d26218d79e531a6a624adca7279
Arkadiy Voronov <voronov_ao@mail.ru>
2020-08-08 09:53:01 +0500

    add fix

--- python_easy_way_to_learn/ex18.py
+++ python_easy_way_to_learn/ex18.py
@@ -2,11 +2,11 @@
 def print_two(*args):
     arg1, arg2 = args
     print(f"arg1: {arg1}, arg2: {arg2}")
-    
+
 # ок, здесь вместо *args мы делаем следующее
 def print_two_again(arg1, arg2):
     print(f"arg1: {arg1}, arg2: {arg2}")
-    
+
 # принимает только один аргумент
 def print_one(arg1):
     print(f"arg1: {arg1}")
@@ -14,8 +14,8 @@ def print_one(arg1):
 # не принимает аргументов
 def print_none():
     print("А я ничего не получаю.")
-    
-    
+
+
 print_two("Михаил","Райтман")
 print_two_again("Михаил","Райтман")
 print_one("Первый!")
