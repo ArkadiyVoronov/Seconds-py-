88e154aa001b957681f98bafa91bfa54578c3cf6
Arkadiy Voronov <voronov_ao@mail.ru>
2020-08-16 18:47:01 +0500

    add fix

--- python_easy_way_to_learn/ex40a.py
+++ python_easy_way_to_learn/ex40a.py
@@ -2,3 +2,7 @@ import mystuff
 
 mystuff.apple()
 print(mystuff.tangerine)
+
+mystuff['яблоко']  # получаем apple из словаря
+mystuff.apple()  # получаем apple из модуля
+mystuff.tangerine  # аналогично, это обычная переменная
