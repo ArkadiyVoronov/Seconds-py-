e8904e77507a1ab61589ea8aa3ea995451a4e8cc
Arkadiy Voronov <voronov_ao@mail.ru>
2020-08-20 08:16:51 +0500

    create oop_intro_init_constructor.py

---
+++ python_workout/oop_intro_init_constructor.py
@@ -0,0 +1,16 @@
+class Staff601:
+    course = '6.01'
+    building = 34
+    room = 501
+
+    def __init__(self, name, role, years, salary):
+        self.name = name
+        self.role = role
+        self.age = years
+        self.salary = salary
+
+    def sulutation(self):
+        return self.role + ' ' + self.name
+        
+pat = Staff601('Pat', 'Professor', 60, 100000)
+print(pat.salutation())
