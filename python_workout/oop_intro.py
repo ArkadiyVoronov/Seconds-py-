6fb8a86ac58ae1ef46ff0a37d81be2f5e2bbbb4f
Arkadiy Voronov <voronov_ao@mail.ru>
2020-08-20 08:04:30 +0500

    add fix

--- python_workout/oop_intro.py
+++ python_workout/oop_intro.py
@@ -2,10 +2,10 @@ class Staff601:
     course = '6.01'
     building = 34
     room =501
-    
+
     def salutation(self):
         return self.role + ' ' + self.name
-        
+
 pat = Staff601()
 print(pat.course)
 
