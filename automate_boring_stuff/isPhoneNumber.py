d363877decb356609617a2274b292d092203d37f
Arkadiy Voronov <voronov_ao@mail.ru>
2020-08-22 14:29:20 +0500

    add fix

--- automate_boring_stuff/isPhoneNumber.py
+++ automate_boring_stuff/isPhoneNumber.py
@@ -11,3 +11,12 @@ def isPhoneNumber(text):
             return False
     if text[7] != '-':
         return False
+    for i in range(8, 12):
+        if not text[i].isdecimal():
+            return False
+    return True
+
+print('Is 415-555-4242 a phone number?')
+print(IsPhoneNumber('415-555-4242'))
+print('Is Moshi moshi a phone number?)
+print(isPhoneNumber('Moshi moshi'))
