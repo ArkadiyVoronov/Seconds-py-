ccdc23f4a027e07587cbc3b13e2f95f3bade8e37
Arkadiy Voronov <voronov_ao@mail.ru>
2020-08-23 19:02:29 +0500

    create enumerate.py

---
+++ python_workout/enumerate.py
@@ -0,0 +1,11 @@
+seasons = ['Spring', 'Summer', 'Fall', 'Winter']
+print(list(enumerate(seasons)))
+print(list(enumerate(seasons, start=1)))
+
+# equivalent to:
+
+def enumerate(sequence, start=0):
+    n = start
+    for elem in sequence:
+        yield n, elem
+        n += 1
