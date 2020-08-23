32aa698fbea364c359b024db0d0f383773ebef09
Arkadiy Voronov <voronov_ao@mail.ru>
2020-08-23 19:04:18 +0500

    add fix

--- python_workout/enumerate.py
+++ python_workout/enumerate.py
@@ -3,9 +3,10 @@ print(list(enumerate(seasons)))
 print(list(enumerate(seasons, start=1)))
 
 # equivalent to:
-
+"""
 def enumerate(sequence, start=0):
     n = start
     for elem in sequence:
         yield n, elem
         n += 1
+"""
