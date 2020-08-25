329af903977aa62cf079bbf7aa6f5be52e0f9f9a
Arkadiy Voronov <voronov_ao@mail.ru>
2020-08-25 21:40:27 +0500

    add fix

--- linux_basic_for_hackers/hacker_arise_script.py
+++ linux_basic_for_hackers/hacker_arise_script.py
@@ -1,14 +1,14 @@
 #! /usr/bin/python3
 
-HackerAriseStringVariable = "Hackers-Arise is the Best Plase to Learn Hacking";
+HackerAriseStringVariable = "Hackers-Arise is the Best Plase to Learn Hacking"
 
 HackerAriseIntegerVariable = 12
 
 HackerAriseFloatingPointVariable = 3.1415
 
-HackersAriseList = [1,2,3,4,5,6}
+HackersAriseList = [1, 2, 3, 4, 5, 6}
 
-HackersAriseDictionary = {'name' : 'OccupytheWeb', 'value' : 27}
+HackersAriseDictionary = {'name': 'OccupytheWeb', 'value': 27}
 
 print(HackersAriseStringVariable)
 
