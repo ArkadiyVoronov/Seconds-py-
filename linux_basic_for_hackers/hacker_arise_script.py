b233f789528cb9372aef8a6b7e4b2a820b2cd8a2
Arkadiy Voronov <voronov_ao@mail.ru>
2020-08-25 21:38:50 +0500

    create hacker_arise_script.py

---
+++ linux_basic_for_hackers/hacker_arise_script.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python3
+
+HackerAriseStringVariable = "Hackers-Arise is the Best Plase to Learn Hacking";
+
+HackerAriseIntegerVariable = 12
+
+HackerAriseFloatingPointVariable = 3.1415
+
+HackersAriseList = [1,2,3,4,5,6}
+
+HackersAriseDictionary = {'name' : 'OccupytheWeb', 'value' : 27}
+
+print(HackersAriseStringVariable)
+
+print(HackersAriseIntegerVariable)
+
+print(HackersAriseFloatingPointVariable)
+
+print(HackersAriseList[3])
