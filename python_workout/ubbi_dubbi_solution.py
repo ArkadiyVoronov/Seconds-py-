f10354c4901b1c8b06a0bf8048d79800aa01f3e7
Arkadiy Voronov <voronov_ao@mail.ru>
2020-08-30 22:06:45 +0500

    add fix

--- python_workout/ubbi_dubbi_solution.py
+++ python_workout/ubbi_dubbi_solution.py
@@ -2,12 +2,12 @@ def ubbi_dubbi(word):
     output = []
     for letter in word:
         if letter in 'aeiou':
-        #  why append to a list, and not to
-        #  a string? To avoid allocating too
-        #  much memory. For short strings,
-        #  it's not a big deal. But for long
-        #  loops and large strings. It's a
-        #  bad idea.
+            """  why append to a list, and not to
+            a string? To avoid allocating too
+            much memory. For short strings,
+            it's not a big deal. But for long
+            loops and large strings. It's a
+            bad idea."""
             output.append(f'up{letter}')
         else:
             output.append(letter)
