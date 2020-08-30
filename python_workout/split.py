066062633b9b42db0d97470389c1102470c9956a
Arkadiy Voronov <voronov_ao@mail.ru>
2020-08-30 12:28:43 +0500

    add fix

--- python_workout/split.py
+++ python_workout/split.py
@@ -3,3 +3,7 @@ print('1,2,3'.split(','))
 print('1,2,3'.split(',', maxsplit=1)
 
 print('1,2,,3,'.split(',')
+
+s = 'abc  def  ghi'
+s.split('  ') #  returns ['abc', ", 'def', ", 'ghi']
+s.split() #  returns ['abc', 'def', 'ghi']
