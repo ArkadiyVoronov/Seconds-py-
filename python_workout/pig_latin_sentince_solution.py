e093963d89c9f6530d39a06bd3082ec6a1cce82a
Arkadiy Voronov <voronov_ao@mail.ru>
2020-08-30 12:35:39 +0500

    add fix

--- python_workout/pig_latin_sentince_solution.py
+++ python_workout/pig_latin_sentince_solution.py
@@ -2,7 +2,7 @@ def pl_sentence(sentence):
     output = []
     for word in sentence.split():
         if word[0] in 'aeiou':
-            output.append(f'{word]way')
+            output.append(f'{word}way')
         else:
             output.append(f'{word[1:]}{word[0]}ay')
 
