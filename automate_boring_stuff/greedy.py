533e07bda9da00a13a88df9365b4a06c465a3957
Arkadiy Voronov <voronov_ao@mail.ru>
2020-09-02 19:35:05 +0500

    create greedy.py

---
+++ automate_boring_stuff/greedy.py
@@ -0,0 +1,7 @@
+greedyHaRegex = re.compile(r'(Ha){3,5}')
+mo1 = greedyHaRegex.search('HaHaHaHaHa')
+print(mo1.group())
+
+nongreedyHaRegex = re.compile(r'(Ha){3,5}?')
+mo2 = nongreedyHaReges.search('HaHaHaHaHa')
+print(mo2.group())
