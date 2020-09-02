9af6403705e271573ec63af3e2803a01c01420aa
Arkadiy Voronov <voronov_ao@mail.ru>
2020-09-02 19:50:23 +0500

    add fix

--- automate_boring_stuff/search_matching_text_only_first_instance.py
+++ automate_boring_stuff/search_matching_text_only_first_instance.py
@@ -3,4 +3,4 @@ import re
 
 phoneNumRegex = re.compile(r' \d\d\d-\d\d\d-\d\d\d\d')
 mo = phoneNumRegex.search('Cell: 415-555-9999 Work: 212-555-0000')
-print(mo.group()) #  will return only '415-555-9999'
+print(mo.group())  #  will return only '415-555-9999'
