3a2326646497cb5b654508ad2774e786f28df90a
Arkadiy Voronov <voronov_ao@mail.ru>
2020-09-03 20:13:10 +0500

    add fix

--- automate_boring_stuff/findall_matching_text.py
+++ automate_boring_stuff/findall_matching_text.py
@@ -1,13 +1,16 @@
 import re
 
-
-phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')  #  has no groups
+#  has no groups
+phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
 print(phoneNumRegex.findall('Cell: 415-555-9999 Work: 212-555-0000'))
-#  will return a list of string matches:
-#  '415-555-9999', '212-555-0000'
+"""  will return a list of string matches:
+'415-555-9999', '212-555-0000'
+"""
 
-phoneNumRegexGroups = re.compile(r'(\d\d\d)-(\d\d\d)-(\d\d\d\d)')  #  has groups
+#  has groups
+phoneNumRegexGroups = re.compile(r'(\d\d\d)-(\d\d\d)-(\d\d\d\d)')
 print(phoneNumRegexGroups.findall('Cell: 415-555-9999 Work: 212-555-0000')
-#  wil return a list of tuples of strings,
-#  (one string for each group):
-#  [('415', '555', '9999'), ('212', '555', '0000')]
+"""  wil return a list of tuples of strings,
+     (one string for each group):
+[('415', '555', '9999'), ('212', '555', '0000')]
+"""
