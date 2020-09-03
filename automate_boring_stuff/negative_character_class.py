5b9b3cb75dbf8d2e2b9d91acc90d90ce23b38f53
Arkadiy Voronov <voronov_ao@mail.ru>
2020-09-03 21:17:18 +0500

    create negative_character_class.py

---
+++ automate_boring_stuff/negative_character_class.py
@@ -0,0 +1,14 @@
+import re
+
+#  matching every character that insn't a vowel
+consonantRegex = re.compile(r'[^aeiouAEIOU]')
+print(consonantRegex.findall('RoboCop eats baby food. BABY FOOD.')
+
+""" will return:
+['R', 'b', 'C', 'p', ' ', 't', 's', ' ', 'b', 'b', 'y', ' ', 'f', 'd', '.',
+' ', 'B', 'B', 'Y', ' ', 'F', 'D', '.']
+
+By placing a caret charactr (^) just after opening bracket,
+you can make a *negative character class*.
+It will match all the characters that are *not* in the character class.
+"""
