496808ff17f8e2a601d677db801f57c917e02ded
Arkadiy Voronov <voronov_ao@mail.ru>
2020-09-03 21:17:47 +0500

    add fix

--- automate_boring_stuff/negative_character_class.py
+++ automate_boring_stuff/negative_character_class.py
@@ -8,7 +8,7 @@ print(consonantRegex.findall('RoboCop eats baby food. BABY FOOD.')
 ['R', 'b', 'C', 'p', ' ', 't', 's', ' ', 'b', 'b', 'y', ' ', 'f', 'd', '.',
 ' ', 'B', 'B', 'Y', ' ', 'F', 'D', '.']
 
-By placing a caret charactr (^) just after opening bracket,
+By placing a caret character (^) just after opening bracket,
 you can make a *negative character class*.
 It will match all the characters that are *not* in the character class.
 """
