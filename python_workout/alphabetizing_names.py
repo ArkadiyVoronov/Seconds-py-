2c7bb4b62c25cbdd7f052f3c091e721c2c6fbed3
Arkadiy Voronov <voronov_ao@mail.ru>
2021-03-20 21:01:07 +0500

    create alphabetizing_names.py

---
+++ python_workout/alphabetizing_names.py
@@ -0,0 +1,7 @@
+PEOPLE = [{'first':'Reuven', 'last':'Lerner',
+    'email':'reuven@lerner.co.il'},
+  {'first':'Donald', 'last':'Trump',
+    'email':'president@whitehouse.gov'},
+  {'first':'Vladimir', 'last':'Putin',
+    'email':'president@kremlin.ru'}
+   ]
